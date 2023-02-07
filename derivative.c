98e66ae7eb7ab7a8d4c450d7bb36311403c53731
alisha2501 <119040876+alisha2501@users.noreply.github.com>
2023-02-07 11:35:19 +0530

    Add files via upload

---
+++ derivative.c
@@ -0,0 +1,18 @@
+//to create table for f(x)=5x3,and find derivative of f(x) wrt x 
+#include<stdio.h>
+#include<math.h>
+int derivative(int arr[]);
+int main(){
+	int arr[]={1,2,3,4,5,6};
+	derivative(arr);
+	return 0;
+}
+int derivative(int arr[]){
+	int i, fx, der;
+	printf("f(x) \t Derivative\n");
+	for(i=1;i<7;i++){
+		fx=5*pow(i,3);
+		der=15*pow(i,2);
+		printf("%d \t %d \n", fx, der);
+	}
+}
